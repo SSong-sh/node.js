1dd4a286716b189553e794d7029747a0c9e0412e
seonghye <tjdgp0817@gmail.com>
2023-06-26 16:15:37 +0900

    홈페이지 홈 만들기

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.listen(8080, function () {
 app.get("/pet", function (요청, 응답) {
   응답.send("펫 용품 쇼핑할 수 있는 페이지입니다.");
 });
-
 /* 누군가가 /pet으로 방문하면 pet 관련된 안내문을 띄어주자 */
 
 app.get("/beauty", function (요청, 응답) {
   응답.send("뷰티 용품을 쇼핑할 수 있는 페이지입니다.");
 });
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+// "/" 홈이라는 뜻
