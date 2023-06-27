07f3541b0b28521afdb37daa82f5c5190ef6466a
seonghye <tjdgp0817@gmail.com>
2023-06-27 13:43:07 +0900

    Mongo DB 설치

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+const MongoClient = require("mongodb").MongoClient;
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
-/* 8080 port로 웹서버를 열고 잘 열리면 listeninf on 8080을 출력해주세요 */
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.10arsak.mongodb.net/?retryWrites=true&w=majority",
+  function (에러, client) {
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+    /* 8080 port로 웹서버를 열고 잘 열리면 listeninf on 8080을 출력해주세요 */
+  }
+);
 
 app.get("/pet", function (요청, 응답) {
   응답.send("펫 용품 쇼핑할 수 있는 페이지입니다.");
