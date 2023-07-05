c43f928f79948ed8fdcde6bcfc2c7f3f5fa89818
seonghye <tjdgp0817@gmail.com>
2023-07-05 14:49:28 +0900

    search.ejs 파일 추가

--- server.js
+++ server.js
@@ -201,6 +201,6 @@ app.get('/search', (요청, 응답)=>{
   console.log(요청.query.value)
   db.collection('post').find({제목 : 요청.query.value}).toArray((에러, 결과)=>{
     console.log(결과)
-    응답.render("result.ejs")
+    응답.render("search.ejs", { posts: 결과 })
   })
 })
