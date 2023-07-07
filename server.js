b14fce932f54c2e28cbe026dd46222eddc9f8199
seonghye <tjdgp0817@gmail.com>
2023-07-07 13:50:49 +0900

    이미지 업로드 + 이미지 서버 만들기

--- server.js
+++ server.js
@@ -218,5 +218,29 @@ app.get("/search", (요청, 응답) => {
     });
 });
 
-app.use("/shop", require("./routes/shop.js"));
-app.use("/board/sub", require("./routes/board.js"));
+// app.use("/shop", require("./routes/shop.js"));
+// app.use("/board/sub", require("./routes/board.js"));
+
+let multer = require("multer");
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/image");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.get("/upload", function (요청, 응답) {
+  응답.render("upload.ejs");
+});
+
+app.post("/upload", upload.single("profile"), function (요청, 응답) {
+  응답.send("업로드완료");
+});
+
+app.get("/image/:imageName", function (요청, 응답) {
+  응답.sendFile(__dirname + "/public/image/" + 요청.params.imageName);
+});
