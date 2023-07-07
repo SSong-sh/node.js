218e5272663f4c5aa65d966ba288a692bfe23a08
seonghye <tjdgp0817@gmail.com>
2023-07-07 10:53:52 +0900

    로그인기능을 포함한 게시판기능

--- server.js
+++ server.js
@@ -62,43 +62,6 @@ app.get("/detail/:id", function (요청, 응답) {
   );
 });
 
-app.post("/add", function (요청, 응답) {
-  응답.send("전송완료");
-  db.collection("counter").findOne(
-    { name: "게시물갯수" },
-    function (에러, 결과) {
-      console.log(결과.totalPost);
-      var 총게시물갯수 = 결과.totalPost;
-      db.collection("post").insertOne(
-        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
-        function (에러, 결과) {
-          console.log("저장완료");
-          //카운터라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 함(수정)
-          db.collection("counter").updateOne(
-            { name: "게시물갯수" },
-            { $inc: { totalPost: 1 } },
-            function (에러, 결과) {
-              if (에러) {
-                return console.log(에러);
-              }
-            }
-          );
-        }
-      );
-    }
-  );
-});
-
-app.delete("/delete", function (요청, 응답) {
-  console.log(요청.body);
-  요청.body._id = parseInt(요청.body._id);
-  //요청.body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해주세요
-  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
-    console.log("삭제완료");
-    응답.status(200).send({ message: "성공했습니다" });
-  });
-});
-
 app.put("/edit", function (요청, 응답) {
   db.collection("post").updateOne(
     { _id: parseInt(요청.body.id) },
@@ -193,16 +156,53 @@ passport.deserializeUser(function (아이디, done) {
   });
 });
 
+app.post("/add", function (요청, 응답) {
+  응답.send("전송완료");
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function (에러, 결과) {
+          console.log("저장완료");
+          //카운터라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 함(수정)
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+app.delete("/delete", function (요청, 응답) {
+  console.log(요청.body);
+  요청.body._id = parseInt(요청.body._id);
+  //요청.body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해주세요
+  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+    console.log("삭제완료");
+    응답.status(200).send({ message: "성공했습니다" });
+  });
+});
+
 app.get("/fail", function (요청, 응답) {
   응답.render("fail.ejs");
 });
 
-app.get('/search', (요청, 응답)=>{
-  console.log(요청.query.value)
-  db.collection('post').find({$text : {$search: 요청.query.value}}).toArray((에러, 결과)=>{
-    console.log(결과)
-    응답.render("search.ejs", { posts: 결과 })
-  })
-})
-
-
+app.get("/search", (요청, 응답) => {
+  console.log(요청.query.value);
+  db.collection("post")
+    .find({ $text: { $search: 요청.query.value } })
+    .toArray((에러, 결과) => {
+      console.log(결과);
+      응답.render("search.ejs", { posts: 결과 });
+    });
+});
