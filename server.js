10465fd06ab0b5f56c94b83e7c503538eee8264d
seonghye <tjdgp0817@gmail.com>
2023-07-18 16:04:29 +0900

    채팅방 만들기

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ app.get("/list", function (요청, 응답) {
   db.collection("post")
     .find()
     .toArray(function (에러, 결과) {
-      console.log(결과);
       응답.render("list.ejs", { posts: 결과 });
     });
 });
@@ -56,7 +55,6 @@ app.get("/detail/:id", function (요청, 응답) {
   db.collection("post").findOne(
     { _id: parseInt(요청.params.id) },
     function (에러, 결과) {
-      console.log(결과);
       응답.render("detail.ejs", { data: 결과 });
     }
   );
@@ -156,29 +154,48 @@ passport.deserializeUser(function (아이디, done) {
   });
 });
 
+app.post("/register", function (요청, 응답) {
+  const id = 요청.body.id;
+  const pw = 요청.body.pw;
+
+  db.collection("login").insertOne({ id: id, pw: pw }, function (에러, 결과) {
+    if (에러) {
+      console.error(에러);
+      return 응답.status(500).send("등록 중 오류가 발생했습니다.");
+    }
+
+    // 등록이 성공한 경우에 대한 처리
+    console.log("등록이 성공했습니다.");
+    응답.redirect("/"); // 등록 성공 후 리다이렉션할 경로 설정
+  });
+});
+
 app.post("/add", function (요청, 응답) {
   응답.send("전송완료");
   db.collection("counter").findOne(
     { name: "게시물갯수" },
     function (에러, 결과) {
-      console.log(결과.totalPost);
       var 총게시물갯수 = 결과.totalPost;
-      db.collection("post").insertOne(
-        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
-        function (에러, 결과) {
-          console.log("저장완료");
-          //카운터라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 함(수정)
-          db.collection("counter").updateOne(
-            { name: "게시물갯수" },
-            { $inc: { totalPost: 1 } },
-            function (에러, 결과) {
-              if (에러) {
-                return console.log(에러);
-              }
+
+      var 저장내용 = {
+        _id: 총게시물갯수 + 1,
+        작성자: 요청.user._id,
+        제목: 요청.body.title,
+        날짜: 요청.body.date,
+      };
+      db.collection("post").insertOne(저장내용, function (에러, 결과) {
+        console.log("저장완료");
+        //카운터라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야 함(수정)
+        db.collection("counter").updateOne(
+          { name: "게시물갯수" },
+          { $inc: { totalPost: 1 } },
+          function (에러, 결과) {
+            if (에러) {
+              return console.log(에러);
             }
-          );
-        }
-      );
+          }
+        );
+      });
     }
   );
 });
@@ -187,8 +204,12 @@ app.delete("/delete", function (요청, 응답) {
   console.log(요청.body);
   요청.body._id = parseInt(요청.body._id);
   //요청.body에 담겨온 게시물 번호를 가진 글을 db에서 찾아서 삭제해주세요
-  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+  var 삭제할데이터 = { _id: 요청.body._id, 작성자: 요청.user.id };
+  db.collection("post").deleteOne(삭제할데이터, function (에러, 결과) {
     console.log("삭제완료");
+    if (에러) {
+      console.log(에러);
+    }
     응답.status(200).send({ message: "성공했습니다" });
   });
 });
@@ -233,6 +254,22 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
+// var path = require('path');
+
+// var upload = multer({
+//     storage: storage,
+//     fileFilter: function (req, file, callback) {
+//         var ext = path.extname(file.originalname);
+//         if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+//             return callback(new Error('PNG, JPG만 업로드하세요'))
+//         }
+//         callback(null, true)
+//     },
+//     limits:{
+//         fileSize: 1024 * 1024
+//     }
+// });
+
 app.get("/upload", function (요청, 응답) {
   응답.render("upload.ejs");
 });
@@ -243,4 +280,18 @@ app.post("/upload", upload.single("profile"), function (요청, 응답) {
 
 app.get("/image/:imageName", function (요청, 응답) {
   응답.sendFile(__dirname + "/public/image/" + 요청.params.imageName);
+});
+
+app.post("/chatroom", function (요청, 응답) {
+  var 저장할거 = {
+    title: "무슨무슨채팅방",
+    member: [요청.body.당한사람id, 요청.user.id],
+    date: new Date(),
+  };
+
+  db.collection("chatroom")
+    .insertOne(저장할거)
+    .then(function (결과) {
+      응답.send("저장완료");
+    });
 });
