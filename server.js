2f116d412deca7ee6bbdebf055cd8d63b755de0f
seonghye <tjdgp0817@gmail.com>
2023-07-25 16:24:38 +0900

    메세지 발행

--- server.js
+++ server.js
@@ -305,3 +305,17 @@ app.get("/chat", 로그인했니, function (요청, 응답) {
       응답.render("chat.ejs", { data: 결과 });
     });
 });
+
+app.post("/message", 로그인했니, function (요청, 응답) {
+  var 저장할거 = {
+    parent: 요청.body.parent,
+    content: 요청.body.content,
+    userid: 요청.user._id,
+    date: new Date(),
+  };
+  db.collection("message")
+    .insertOne(저장할거)
+    .then(() => {
+      console.log("db저장성공");
+    });
+});
